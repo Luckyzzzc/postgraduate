27d8ccb37a5de62881a30a41f209f8d5179f204c
13110520383 <gogog0go@jobmail.vip>
2020-05-31 20:08:55 +0800

    map错误
    忘记写carousel_id的前缀了。。。

--- src/main/java/com/example/postgraduate/Dao/CarouselMapper.java
+++ src/main/java/com/example/postgraduate/Dao/CarouselMapper.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.Update;
 import org.springframework.stereotype.Repository;
@@ -24,12 +25,12 @@ public interface CarouselMapper {
     		+ "`carousel`.`title` = #{title},"
     		+ "`carousel`.`content` = #{content} "
     		+ " WHERE `carousel`.`carousel_id` = #{carousel_id}")
-    boolean updateCarousel(Integer carousel_id, String url, String title,String content);
+    boolean updateCarousel(@Param("carousel_id") Integer carousel_id,@Param("url") String url,@Param("title") String title,@Param("content") String content);
 
     @Delete("DELETE FROM `postgraduate`.`carousel` WHERE `carousel`.`carousel_id` = #{carousel_id}")
     boolean deleteCarousel(Integer carousel_id);
 
-    @Select("SELECT * FROM `postgraduate`.`carousel` WHERE `carousel`.`carousel` = #{carousel_id}")
+    @Select("SELECT * FROM `postgraduate`.`carousel` WHERE `carousel`.`carousel_id` = #{carousel_id}")
     Carousel getCarousel(Integer carousel_id);
     
     @Select("SELECT * FROM `postgraduate`.`carousel`")
